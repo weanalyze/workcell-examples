4b1f6b1d7451d4ad6688d074ad04304180097987
jiandong <jiandongj@qq.com>
2023-01-29 22:11:50 +0800

    update basic examples;

--- basic/hello_pandas/app.py
+++ basic/hello_pandas/app.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
 import pandas as pd
-from workcell.integrations.types import DataFrameOutput
+from workcell.integrations.types import PandasDataFrame
+
 
 class DummyInput(BaseModel):
     pass
 
-def hello_dataframe(input: DummyInput) -> DataFrameOutput:
+def hello_dataframe(input: DummyInput) -> PandasDataFrame:
     """Returns a pandas DataFrame in Arrow format."""
     d = {'col1': [1, 2, 3], 'col2': ['a', 'b', 'c'], 'col3': [None, 0.1, -1.5]}
     df = pd.DataFrame(data=d)
-    output = DataFrameOutput(data=df)
+    output = PandasDataFrame(data=df)
     return output
