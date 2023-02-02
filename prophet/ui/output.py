31f964df5a1b9566ddfd90d4257d4dae6648e35d
jiandong <jiandongj@qq.com>
2023-02-02 19:29:35 +0800

    refactor prophet;

--- prophet/ui/output.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class ProphetOutput(BaseModel):
-    short_text: str = Field(..., max_length=60, description="Short text property")
-    long_text: str = Field(..., description="Unlimited text property")
